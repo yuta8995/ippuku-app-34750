837b8a314ae02c73fcf14f75ebf101b9238dbd51
yuta8995 <spcrnjmk9705@gmail.com>
2021-08-02 03:12:20 +0900

    users_controller.rbを作成、showアクション定義、ユーザー名表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user)
   end
 
   def show
