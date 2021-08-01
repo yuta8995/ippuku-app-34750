bf096c51555c873e0b45970402de896c005db169
yuta8995 <spcrnjmk9705@gmail.com>
2021-08-02 03:29:00 +0900

    投稿画面のビュー変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,7 +33,7 @@ class TweetsController < ApplicationController
   private
   
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_204855) do
+ActiveRecord::Schema.define(version: 2021_08_01_182356) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
