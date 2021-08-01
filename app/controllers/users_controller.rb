2854a4d7bb8577909fd1a79500609bf2e8705159
yuta8995 <spcrnjmk9705@gmail.com>
2021-08-02 03:53:36 +0900

    投稿一覧の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets
   end
-  
+
 end
